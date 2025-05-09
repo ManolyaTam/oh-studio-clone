a550f386f0a0e660f0fbc904ed8a489f50be938c
Manolya Tamimi <tamimimanolya@gmail.com>
2025-05-09 13:46:08 +0300

    fixing an error

--- src/components/SpinningCross/SpinningCross.jsx
+++ src/components/SpinningCross/SpinningCross.jsx
@@ -3,7 +3,7 @@ import "./spinning-cross.css";
 const SpinningCross = () => {
   return (
     <div className="logo">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 800" width="26" height="26" preserveAspectRatio="xMidYMid slice" class="spinning-cross">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 800" width="26" height="26" preserveAspectRatio="xMidYMid slice" className="spinning-cross">
         <g>
           <path
             d="M156.74,156.77 L643.26,643.23
@@ -11,9 +11,9 @@ const SpinningCross = () => {
          M56,400 L744,400
          M400,56 L400,744"
             stroke="black"
-            stroke-width="176"
+            strokeWidth="176"
             fill="none"
-            stroke-linecap="butt"
+            strokeLinecap="butt"
           />
         </g>
       </svg>
