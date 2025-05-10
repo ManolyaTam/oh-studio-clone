879bec3460d9b0ab023117a4906ccddaeae56de0
Manolya Tamimi <tamimimanolya@gmail.com>
2025-05-10 12:46:48 +0300

    SOL-9: Haptic hero heading ready

---
+++ src/components/HapticHeader/HapticHeader.jsx
@@ -0,0 +1,13 @@
+import "./haptic-header.css"
+
+const HapticHeader = () => {
+  return (
+    <div id="hero" className="haptic-header">
+      <h1>Haptic</h1>
+      <p>
+        We help ambitious<br />teams turn bold visions<br /> into lasting impact      </p>
+    </div>
+  )
+}
+
+export default HapticHeader
--- src/pages/HapticStudio/HapticStudio.jsx
+++ src/pages/HapticStudio/HapticStudio.jsx
@@ -1,16 +1,15 @@
 import "./footer.css"
+import Header from "../../components/HapticHeader/HapticHeader.jsx";
 import Pricing from "../../components/Pricing/Pricing.jsx";
 import Careers from "../../components/Careers/Careers.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
-import DShapeAnimation from "../../components/DShapeAnimation/DShapeAnimation.jsx";
 
 const HapticStudio = () => {
   return (
     <div className="haptic-studio">
-      <div className="haptic-header"></div>
+      <Header />
       <div className="haptic-content">
         <Pricing />
-        <DShapeAnimation />
         <Careers />
       </div>
       <Footer />
