58aac4956db6c4b8c984ec8f501d1c4004ad2604
Manolya Tamimi <tamimimanolya@gmail.com>
2025-05-13 20:38:07 +0300

    SOL-17: fixing images css (transition), moving data to seperate file

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -1,50 +1,6 @@
 import "./partners.css";
 import BulletPoint from "../common/BulletPoint";
-
-const partnersData = [
-  {
-    id: 1,
-    name: "Momentify",
-    details: "Brand, Web, Product",
-    year: 2024,
-    image: "https://framerusercontent.com/images/WtDkhEjQ1TNDVDYNkBVhFoJnfU.jpg?scale-down-to=1024",
-  },
-  {
-    id: 2,
-    name: "Norby",
-    details: "Motion",
-    year: 2025,
-    image: "https://framerusercontent.com/images/WtDkhEjQ1TNDVDYNkBVhFoJnfU.jpg?scale-down-to=1024",
-  },
-  {
-    id: 3,
-    name: "Spark",
-    details: "Brand, Web, Product",
-    year: 2024,
-    image: "https://framerusercontent.com/images/5z0ZyBUqdAoedRuSZyNWGGNEzLs.jpg?scale-down-to=1024",
-  },
-  {
-    id: 4,
-    name: "Google",
-    details: "Web, Product",
-    year: 2024,
-    image: "https://framerusercontent.com/images/WtDkhEjQ1TNDVDYNkBVhFoJnfU.jpg?scale-down-to=1024",
-  },
-  {
-    id: 5,
-    name: "Harvard",
-    details: "Brand, Web, Product",
-    year: 2025,
-    image: "https://framerusercontent.com/images/WtDkhEjQ1TNDVDYNkBVhFoJnfU.jpg?scale-down-to=1024",
-  },
-  {
-    id: 6,
-    name: "Neon Horizons",
-    details: "Brand, Product",
-    year: 2025,
-    image: "https://framerusercontent.com/images/WtDkhEjQ1TNDVDYNkBVhFoJnfU.jpg?scale-down-to=1024",
-  },
-];
+import { partnersData } from "../../services/partnersData";
 
 const Partners = () => {
   return (
@@ -65,6 +21,7 @@ const Partners = () => {
                 width="500px"
                 height="370px"
                 data-image={partner.image}
+                data-order={partner.order}
               />
             </div>
           </li>
