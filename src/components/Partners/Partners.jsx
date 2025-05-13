4665bd9ab59cd6ca36f79e55e02ba9b8d689f80f
Manolya Tamimi <tamimimanolya@gmail.com>
2025-05-13 20:09:13 +0300

    SOL-17: adding images

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -10,37 +10,97 @@ const Partners = () => {
         <li>
           <div className="partner">
             <h3>Momentify</h3>
-            <p className="partner-details"><span>Brand, Web, Product</span> <span>2024</span></p>
+            <p className="partner-details">
+              <span>Brand, Web, Product</span><span>2024</span>
+            </p>
+            <img
+              src="https://framerusercontent.com/images/WtDkhEjQ1TNDVDYNkBVhFoJnfU.jpg?scale-down-to=1024"
+              alt="Momentify"
+              className="partner-image"
+              width="500px"
+              height="370px"
+              data-image="https://framerusercontent.com/images/WtDkhEjQ1TNDVDYNkBVhFoJnfU.jpg?scale-down-to=1024"
+            />
           </div>
         </li>
         <li>
           <div className="partner">
             <h3>Norby</h3>
-            <p className="partner-details"><span>Motion</span> <span>2025</span></p>
+            <p className="partner-details">
+              <span>Motion</span><span>2025</span>
+            </p>
+            <img
+              src="https://framerusercontent.com/images/WtDkhEjQ1TNDVDYNkBVhFoJnfU.jpg?scale-down-to=1024"
+              alt="Norby"
+              className="partner-image"
+              width="500px"
+              height="370px"
+              data-image="https://framerusercontent.com/images/WtDkhEjQ1TNDVDYNkBVhFoJnfU.jpg?scale-down-to=1024"
+            />
           </div>
         </li>
         <li>
           <div className="partner">
             <h3>Spark</h3>
-            <p className="partner-details"><span>Brand, Web, Product</span> <span>2024</span></p>
+            <p className="partner-details">
+              <span>Brand, Web, Product</span><span>2024</span>
+            </p>
+            <img
+              src="https://framerusercontent.com/images/5z0ZyBUqdAoedRuSZyNWGGNEzLs.jpg?scale-down-to=1024"
+              alt="Spark"
+              className="partner-image"
+              width="500px"
+              height="370px"
+              data-image="https://framerusercontent.com/images/5z0ZyBUqdAoedRuSZyNWGGNEzLs.jpg?scale-down-to=1024"
+            />
           </div>
         </li>
         <li>
           <div className="partner">
             <h3>Google</h3>
-            <p className="partner-details"><span>Web, Product</span> <span>2024</span></p>
+            <p className="partner-details">
+              <span>Web, Product</span><span>2024</span>
+            </p>
+            <img
+              src="https://framerusercontent.com/images/WtDkhEjQ1TNDVDYNkBVhFoJnfU.jpg?scale-down-to=1024"
+              alt="Google"
+              className="partner-image"
+              width="500px"
+              height="370px"
+              data-image="https://framerusercontent.com/images/WtDkhEjQ1TNDVDYNkBVhFoJnfU.jpg?scale-down-to=1024"
+            />
           </div>
         </li>
         <li>
           <div className="partner">
             <h3>Harvard</h3>
-            <p className="partner-details"><span>Brand, Web, Product</span> <span>2025</span></p>
+            <p className="partner-details">
+              <span>Brand, Web, Product</span><span>2025</span>
+            </p>
+            <img
+              src="https://framerusercontent.com/images/WtDkhEjQ1TNDVDYNkBVhFoJnfU.jpg?scale-down-to=1024"
+              alt="Harvard"
+              className="partner-image"
+              width="500px"
+              height="370px"
+              data-image="https://framerusercontent.com/images/WtDkhEjQ1TNDVDYNkBVhFoJnfU.jpg?scale-down-to=1024"
+            />
           </div>
         </li>
         <li>
           <div className="partner">
             <h3>Neon Horizons</h3>
-            <p className="partner-details"><span>Brand, Product</span> <span>2025</span></p>
+            <p className="partner-details">
+              <span>Brand, Product</span><span>2025</span>
+            </p>
+            <img
+              src="https://framerusercontent.com/images/WtDkhEjQ1TNDVDYNkBVhFoJnfU.jpg?scale-down-to=1024"
+              alt="Neon Horizons"
+              className="partner-image"
+              width="500px"
+              height="370px"
+              data-image="https://framerusercontent.com/images/WtDkhEjQ1TNDVDYNkBVhFoJnfU.jpg?scale-down-to=1024"
+            />
           </div>
         </li>
       </ul>
